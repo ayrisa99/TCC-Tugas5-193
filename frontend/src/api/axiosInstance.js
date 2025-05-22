afac7810e286b199880c6ab284a50062b685c5ea
ayrisatrianida <ayrisatrianida04@gmail.com>
2025-05-22 18:27:16 +0700

    tambah dikit

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -13,7 +13,7 @@ instance.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("accessToken");
     if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
+      config.headers.Authorization = `Bearer ${accessToken}`;
     }
     return config;
   },
