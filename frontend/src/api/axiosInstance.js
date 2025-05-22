3c7d9106dbab79f897eec770c18d9166fee11c86
ayrisatrianida <ayrisatrianida04@gmail.com>
2025-05-22 16:27:38 +0700

    tambah lagi

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,11 +1,23 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://backend-193-174534490336.us-central1.run.app/", // Ganti dengan URL backend kamu
+  baseURL: "https://backend-193-174534490336.us-central1.run.app/",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
   },
 });
+
+// Interceptor untuk menambahkan token Authorization
+instance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("accessToken");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default instance;
